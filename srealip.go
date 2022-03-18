b75cfcabdcbd5926e5610beb602a60277a8abb85
ido <FaranIdoF@gmail.com>
2022-03-18 16:22:50 +0200

    finalize code and UT

--- srealip.go
+++ srealip.go
@@ -29,34 +29,52 @@ func extractIpFromRemoteAddr(remoteAddr string) string {
 }
 
 // SecureRealIP returns the trusted non-private real IP address from input request.
-// Note:
-// 1. This IP can be trusted only if your server is behind reverse proxy such as AWS ELB/ALB.
-// 2. If all address in `X-Forwarded-For` are private - use http RemoteAddr
+// Note: This IP can be trusted only if your server is behind reverse proxy such as AWS ELB/ALB.
 func SecureRealIP(r *http.Request) string {
 	xForwardedFor := r.Header.Values("X-Forwarded-For")
 
-	// go over xForwardedFor from right to left
+	// go over xForwardedFor from right to left, return the rightmost non private IP address
 	for i := len(xForwardedFor) - 1; i >= 0; i-- {
 		value := strings.TrimSpace(xForwardedFor[i])
 		realIP := net.ParseIP(value)
 
-		// skip non IP address in xForwardedFor
-		if realIP == nil {
-			continue
-		}
-
-		// skip private addresses
-		if isPrivateIP(realIP) {
+		// skip non IP strings or private IP addresses in xForwardedFor
+		if realIP == nil || isPrivateIP(realIP) {
 			continue
 		}
 
 		return realIP.String()
 	}
 
-	// all address in `X-Forwarded-For` are private - return the rightmost address
+	// all address in `X-Forwarded-For` are private or invaid - return HTTP RemoteAddr
 	return extractIpFromRemoteAddr(r.RemoteAddr)
 }
 
+// NaiveRealIP returns the most real non-private IP address ("closest to client") from input request.
+// Note: This IP can be spoofed by malicious sensder, so avoid using it for security purposes
 func NaiveRealIP(r *http.Request) string {
-	return "bobo"
+	// X-Real-IP header should contain only one value
+	xRealIPHeader := r.Header.Get("X-Real-IP")
+	xRealIP := net.ParseIP(xRealIPHeader)
+
+	if xRealIP != nil && !isPrivateIP(xRealIP) {
+		return xRealIPHeader
+	}
+
+	xForwardedFor := r.Header.Values("X-Forwarded-For")
+	// go over xForwardedFor from left to right, return the leftmost non private IP address
+	for i := 0; i < len(xForwardedFor); i++ {
+		value := strings.TrimSpace(xForwardedFor[i])
+		realIP := net.ParseIP(value)
+
+		// skip non IP strings or private IP addresses in xForwardedFor
+		if realIP == nil || isPrivateIP(realIP) {
+			continue
+		}
+
+		return realIP.String()
+	}
+
+	// all address in `X-Forwarded-For` and 'X-Real-IP' are private or empty - return HTTP RemoteAddr
+	return extractIpFromRemoteAddr(r.RemoteAddr)
 }
--- srealip_test.go
+++ srealip_test.go
@@ -10,18 +10,14 @@ import (
 
 func newHttpRequest(remoteAddr string, xRealIP string, xForwardedFor ...string) *http.Request {
 	h := http.Header{
-		"X-Real-IP":       []string{xRealIP},
 		"X-Forwarded-For": xForwardedFor,
 	}
+	h.Set("X-Real-IP", xRealIP)
 
 	return &http.Request{
 		RemoteAddr: remoteAddr,
 		Header:     h,
 	}
-}
-
-type testStruct struct {
-	name string
 }
 
 func TestIsPrivate(t *testing.T) {
@@ -42,6 +38,9 @@ func TestIsPrivate(t *testing.T) {
 
 	invalidAddr := "string"
 	assert.False(t, isPrivateIP(net.ParseIP(invalidAddr)))
+
+	emptyAddr := ""
+	assert.False(t, isPrivateIP(net.ParseIP(emptyAddr)))
 }
 
 func TestSecureRealIp(t *testing.T) {
@@ -50,19 +49,23 @@ func TestSecureRealIp(t *testing.T) {
 	publicAddr3 := "119.15.55.11"
 	localAddr := "127.0.0.0"
 	privateAddr := "192.168.1.1"
+	invalidAddr := "invalidStr"
 
 	tests := map[string]struct {
 		request  *http.Request
 		expected string
 	}{
-		"No X-Forwarded-For":             {request: newHttpRequest(publicAddr1, ""), expected: publicAddr1},
-		"X-Forwarded-For - one value":    {request: newHttpRequest(publicAddr1, "", publicAddr2), expected: publicAddr2},
-		"multiple X-Forwarded-For":       {request: newHttpRequest(publicAddr3, "", localAddr, publicAddr1, publicAddr2), expected: publicAddr2},
-		"Has local X-Forwarded-For":      {request: newHttpRequest(publicAddr3, "", publicAddr1, localAddr), expected: publicAddr1},
-		"Has private X-Forwarded-For":    {request: newHttpRequest(publicAddr3, "", publicAddr1, localAddr, privateAddr), expected: publicAddr1},
-		"Has X-Real-IP":                  {request: newHttpRequest(publicAddr3, publicAddr2, publicAddr1, localAddr), expected: publicAddr1},
-		"not IP X-Forwarded-For":         {request: newHttpRequest(publicAddr3, "", "testString"), expected: publicAddr3},
-		"not IP X-Forwarded-For then IP": {request: newHttpRequest(publicAddr3, "", publicAddr1, localAddr, "testString"), expected: publicAddr1},
+		"No X-Forwarded-For":              {request: newHttpRequest(publicAddr1, ""), expected: publicAddr1},
+		"X-Forwarded-For - one value":     {request: newHttpRequest(publicAddr1, "", publicAddr2), expected: publicAddr2},
+		"multiple X-Forwarded-For":        {request: newHttpRequest(publicAddr3, "", localAddr, publicAddr1, publicAddr2), expected: publicAddr2},
+		"Has local X-Forwarded-For":       {request: newHttpRequest(publicAddr3, "", publicAddr1, localAddr), expected: publicAddr1},
+		"Has private X-Forwarded-For":     {request: newHttpRequest(publicAddr3, "", publicAddr1, localAddr, privateAddr), expected: publicAddr1},
+		"Has X-Real-IP":                   {request: newHttpRequest(publicAddr3, publicAddr2, publicAddr1, localAddr), expected: publicAddr1},
+		"not IP X-Forwarded-For":          {request: newHttpRequest(publicAddr3, "", invalidAddr), expected: publicAddr3},
+		"not + vallid IP X-Forwarded-For": {request: newHttpRequest(publicAddr3, "", publicAddr2, privateAddr, invalidAddr), expected: publicAddr2},
+		"not IP X-Forwarded-For then IP":  {request: newHttpRequest(publicAddr3, "", publicAddr1, localAddr, invalidAddr), expected: publicAddr1},
+		"invalid at all header":           {request: newHttpRequest(invalidAddr, invalidAddr, invalidAddr), expected: invalidAddr},
+		"empty IP at all header":          {request: newHttpRequest("", ""), expected: ""},
 	}
 
 	for name, tc := range tests {
@@ -72,4 +75,58 @@ func TestSecureRealIp(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestNaiveRealIp(t *testing.T) {
+	publicAddr1 := "144.12.54.87"
+	publicAddr2 := "119.14.55.11"
+	publicAddr3 := "119.15.55.11"
+	publicAddr4 := "119.16.55.11"
+	localAddr := "127.0.0.0"
+	privateAddr := "192.168.1.1"
+	invalidAddr := "invalidStr"
+
+	tests := map[string]struct {
+		request  *http.Request
+		expected string
+	}{
+		"No X-Forwarded-For":              {request: newHttpRequest(publicAddr1, ""), expected: publicAddr1},
+		"X-Forwarded-For - one value":     {request: newHttpRequest(publicAddr1, "", publicAddr2), expected: publicAddr2},
+		"multiple X-Forwarded-For":        {request: newHttpRequest(publicAddr3, "", localAddr, publicAddr1, publicAddr2), expected: publicAddr1},
+		"Has private X-Forwarded-For":     {request: newHttpRequest(publicAddr3, "", privateAddr, publicAddr1, localAddr), expected: publicAddr1},
+		"not IP X-Forwarded-For":          {request: newHttpRequest(publicAddr3, "", invalidAddr), expected: publicAddr3},
+		"not + vallid IP X-Forwarded-For": {request: newHttpRequest(publicAddr3, "", privateAddr, publicAddr2, invalidAddr), expected: publicAddr2},
+		"not IP X-Forwarded-For then IP":  {request: newHttpRequest(publicAddr3, "", localAddr, invalidAddr, publicAddr1), expected: publicAddr1},
+		"Has X-Real-IP":                   {request: newHttpRequest(publicAddr3, publicAddr2, publicAddr1, localAddr), expected: publicAddr2},
+		"Private X-Real-IP":               {request: newHttpRequest(publicAddr3, privateAddr, localAddr, publicAddr1), expected: publicAddr1},
+		"Private Headers":                 {request: newHttpRequest(publicAddr3, privateAddr, localAddr, privateAddr), expected: publicAddr3},
+		"Invalid X-Real-IP":               {request: newHttpRequest(publicAddr3, invalidAddr, publicAddr1), expected: publicAddr1},
+		"Invalid Headers":                 {request: newHttpRequest(publicAddr3, invalidAddr, invalidAddr), expected: publicAddr3},
+		"empty IP at all header":          {request: newHttpRequest("", ""), expected: ""},
+		"invalid at all header":           {request: newHttpRequest(invalidAddr, invalidAddr, invalidAddr), expected: invalidAddr},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			if actual := NaiveRealIP(tc.request); tc.expected != actual {
+				t.Errorf("expected %s but got %s", tc.expected, actual)
+			}
+		})
+	}
+
+	// Test multiple "X-Real-IP"
+	req := newHttpRequest(publicAddr3, publicAddr2, publicAddr1)
+	req.Header.Add("X-Real-IP", publicAddr4)
+	assert.Equal(t, publicAddr2, NaiveRealIP(req))
+
+	// Test multiple "X-Real-IP", with invalid value as second
+	// NaiveRealIP should only look on the first value
+	req = newHttpRequest(publicAddr3, publicAddr2, publicAddr1)
+	req.Header.Add("X-Real-IP", invalidAddr)
+	assert.Equal(t, publicAddr2, NaiveRealIP(req))
+
+	// Test multiple "X-Real-IP", with invalid value as first
+	req = newHttpRequest(publicAddr3, invalidAddr, publicAddr1)
+	req.Header.Add("X-Real-IP", publicAddr2)
+	assert.Equal(t, publicAddr1, NaiveRealIP(req))
 }
