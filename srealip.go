b2e4ef6c9660fca463f3cfe4e2177370d9d7d14f
aviv.shalom <aviv.shalom@appsflyer.com>
2022-10-18 09:15:22 +0300

    deleted utils.go and moved isInSharedAddressSpace function  to sreailip.go

--- srealip.go
+++ srealip.go
@@ -78,3 +78,16 @@ func NaiveRealIP(r *http.Request) string {
 	// all address in `X-Forwarded-For` and 'X-Real-IP' are private or empty - return HTTP RemoteAddr
 	return extractIpFromRemoteAddr(r.RemoteAddr)
 }
+
+// isInSharedAddressSpace reports whether ip is in the shared address space, according to
+// RFC 6598- IANA-Reserved IPv4 Prefix for Shared Address Space
+func isInSharedAddressSpace(ip net.IP) bool {
+	if ip4 := ip.To4(); ip4 != nil {
+		// Following RFC 6598. Shared Address Space which says:
+		//   The Internet Assigned Numbers Authority (IANA) has reserved the
+		//   following block of IP address space for shared internets:
+		//     100.64.0.0    -   100.127.255.255  (100.64/10 prefix)
+		return ip4[0] == 100 && ip4[1]&0xc0 == 64
+	}
+	return false
+}
